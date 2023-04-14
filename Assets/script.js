8c4db7c6a5f5a86524ab76c07b6a24dfb0359d4f
lkrum <leahkrumholz73@gmail.com>
2023-04-13 22:35:37 -0500

    added day of the week and timer and finished Readme

--- Assets/script.js
+++ Assets/script.js
@@ -11,44 +11,47 @@ var currentTime;
 var eventTime;
 var eventList;
 
-  // event listener for save button. Jessica Saddington, Emma Carr, and Diem Ly helped me come up with this
-  $('.saveBtn').click(function () {
-      eventList = $(this).siblings('.description').val();
-      eventTime = $(this).parent().attr('id');
+// event listener for save button. Jessica Saddington, Emma Carr, and Diem Ly helped me come up with this
+$('.saveBtn').click(function () {
+  eventList = $(this).siblings('.description').val();
+  eventTime = $(this).parent().attr('id');
 
-      localStorage.setItem(eventTime, eventList);
-  });
-
-
-  // colors depicting past(pink), present(blue), and future(green) times.
-  $('.time-block').each(function () {
-    currentTime = dayjs().hour();
-    eventTime = parseInt($(this).attr('id').split('-')[1]);
-
-    if (eventTime === currentTime) {
-      $(this).addClass('present');
-    }
-    else if (eventTime < currentTime) {
-      $(this).addClass('past');
-    }
-    else
-      $(this).addClass('future');
-  });
+  localStorage.setItem(eventTime, eventList);
+});
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+// colors depicting past(pink), present(blue), and future(green) times.
+$('.time-block').each(function () {
+  currentTime = dayjs().hour();
+  eventTime = parseInt($(this).attr('id').split('-')[1]);
 
-  
-  // Jessica Saddington helped me with this for-loop
-  for (i = 9; i <= 17; i++) {
-    $(`#hour - ${i}.eventTime`).val(localStorage.getItem(`hour - ${i}`));
-  };
-
-  // current date 
-  function showDate() {
-    dateToday = dayjs().format('MMM DD, YYYY');
-    timeDisplayEl.text(dateToday);
+  if (eventTime === currentTime) {
+    $(this).addClass('present');
   }
+  else if (eventTime < currentTime) {
+    $(this).addClass('past');
+  }
+  else
+    $(this).addClass('future');
+});
 
-  showDate();
+
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+
+
+// Jessica Saddington helped me with this for-loop
+for (i = 9; i <= 17; i++) {
+  $(`#hour - ${i}.eventTime`).val(localStorage.getItem(`hour - ${i}`));
+};
+
+// current date 
+function showDate() {
+  dateToday = dayjs().format('dddd MMM DD, YYYY hh:mm:ss');
+  timeDisplayEl.text(dateToday);
+}
+
+showDate();
+setInterval(showDate, 1000);
+
+
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Work-Day Calendar
 
 ## Description
-I've created an interactive calendar where you can save your events. The colors of events change depending on the time of day. The background colors change depending on whether the event is in the past, present, for future. The color is pink for the past, blue for the present, and green for the future.
+I've created an interactive, color-coded calendar where you can save your events for the workday. The background colors change depending on whether the event is in the past (pink), present (blue), or future (green). 
 
 ![Main](./Assets/Calendar-main.png)
 
-Link to live page: 
+Link to live page: https://lkrum.github.io/Work-Day-Scheduler/
 
 ## Installation
 N/A
@@ -14,11 +14,14 @@ N/A
 Click into one of the table rows to log your events for the day. Click the save button on the right side to save your text input.
 
 ## Credits 
+I used Bootstrap, Jquery, and Day.js to make my application.
 
-- [W3schools](https://www.w3schools.com/jsref/met_loc_assign.asp
-- [W3schools]https://www.w3schools.com/jsref/event_target.asp)
-- [W3schools](https://www.w3schools.com/tags/tag_input.asp)
+- [W3schools](https://www.w3schools.com/jsref/jsref_split.asp)
+- [W3schools](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
+- [W3schools](https://www.w3schools.com/jquery/jquery_events.asp)
 - [W3schools](https://www.w3schools.com/tags/tag_button.asp)
+- [mozilla](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
+- [Jquery](https://api.jquery.com/attr/)
 - Diem Ly, one of the class TAs, helped me write the formula for getting the save button to function.
 - My classmates Emma Carr and Jessica Saddington also helped me with the save button, JQuery formatting, and the hour id for-loop.
 
