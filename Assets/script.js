96c3df48eea43048f3ecc9dc62653a4bac229ad1
lkrum <leahkrumholz73@gmail.com>
2023-04-13 21:23:15 -0500

    troubleshooted local storage not retrieving data

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,19 @@
 // read from localStorage
 // Populate page with data if exists in localStorage
 
-// click event with event delegation to target the text area associated with the save button
-// save button has to read value of time associated with it
-// use the sibling DOM method to traverse the DOM, event.target parent/sibling (will get you actual save button)
-// capture the value of the text area, save string to local storage
-// make sure function is reusable
-
-// Use Dayjs to get current time use military time 
-//  read the value of the time block from the html
-//  need if statement that compares the values and adds a class to change the color
-// loop through html element and read a data-attribute time
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-
 // variable expressions
 var timeDisplayEl = $('#currentDay');
 var saveBtn = $('.saveBtn');
-var timeblockEl = $('.time-block');
 
 // variables
 var dateToday;
-var saveSchedule;
 var currentTime;
 var eventTime;
 var eventList;
-var userInput;
-var hourLoop;
-
-$(function () {
 
   // event listener for save button. Jessica Saddington, Emma Carr, and Diem Ly helped me come up with this
   $('.saveBtn').click(function () {
@@ -57,14 +39,12 @@ $(function () {
       $(this).addClass('future');
   });
 
-  console.log(eventTime)
-  console.log(currentTime)
-  console.log(eventTime < currentTime)
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
 
-  // Jessica Saddington helped me with this for loop
+  
+  // Jessica Saddington helped me with this for-loop
   for (i = 9; i <= 17; i++) {
     $(`#hour - ${i}.eventTime`).val(localStorage.getItem(`hour - ${i}`));
   };
@@ -76,5 +56,3 @@ $(function () {
   }
 
   showDate();
-
-});
